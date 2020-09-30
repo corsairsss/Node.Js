e66191d43c8bae237a03296f55b95707f4b5ed8b
Osaulenko Oleksandr <oom2212@gmail.com>
2020-09-30 18:51:38 +0300

    add screen

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -1,19 +1,15 @@
 const Joi = require('joi');
 const { ObjectId } = require('mongodb');
-const bcryptjs = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const {
-  UnauthorizedError,
-  NotFoundError,
-} = require("../helpers/error.js");
-
+const { UnauthorizedError } = require('../helpers/error.js');
 const usersModel = require('./user.model.js');
 
 async function getUsers(req, res, next) {
   try {
     const users = await usersModel.find();
-    const filtredUsers=getSomeField(users);
+    const filtredUsers = getSomeField(users);
     return res.status(200).json(filtredUsers);
   } catch (err) {
     next(err);
@@ -29,10 +25,13 @@ async function getUserById(req, res, next) {
     next(err);
   }
 }
-async function getCurrentUser(req,res,next) {
-
-  const filtredUsers=getSomeField([req.user]);
+async function getCurrentUser(req, res, next) {
+  try {
+    const filtredUsers = getSomeField([req.user]);
     return res.status(200).json(filtredUsers[0]);
+  } catch (err) {
+    next(err);
+  }
 }
 
 async function deleteUser(req, res, next) {
@@ -46,49 +45,45 @@ async function deleteUser(req, res, next) {
 }
 async function addNewUser(req, res, next) {
   try {
-
-    const {email,password}=req.body;
+    const { email, password } = req.body;
     const passwordHash = await bcryptjs.hash(password, 4);
 
-      const isEmailExist=await usersModel.findUserByEmail(email);
+    const isEmailExist = await usersModel.findUserByEmail(email);
 
-      if (isEmailExist) return res.status(409).json({"message": "Email in use"})
+    if (isEmailExist) return res.status(409).json({ message: 'Email in use' });
 
     const user = await usersModel.create({
       email,
-      password:passwordHash,
+      password: passwordHash,
     });
 
     return res.status(201).json({
       email: user.email,
-      subscription:user.subscription
-
+      subscription: user.subscription,
     });
   } catch (err) {
     next(err);
   }
 }
 
-
 async function checkUser(email, password) {
   const user = await usersModel.findUserByEmail(email);
   if (!user) {
     throw new UnauthorizedError('Authentication failed');
   }
-  
+
   const isPasswordValid = await bcryptjs.compare(password, user.password);
   if (!isPasswordValid) {
     throw new UnauthorizedError('Authentication failed');
   }
-  
+
   const token = await jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: 10000000, 
+    expiresIn: 10000000,
   });
   await usersModel.updateToken(user._id, token);
-  
+
   return token;
 }
-
 
 async function signIn(req, res, next) {
   try {
@@ -96,13 +91,13 @@ async function signIn(req, res, next) {
     const token = await checkUser(email, password);
     const userId = await jwt.verify(token, process.env.JWT_SECRET).id;
     const user = await usersModel.findOne({ _id: userId });
-    return res.status(200).json({ token,"user":{email,subscription: user.subscription} });
+    return res
+      .status(200)
+      .json({ token, user: { email, subscription: user.subscription } });
   } catch (err) {
     next(err);
   }
 }
-
-
 
 async function updateUser(req, res, next) {
   try {
@@ -125,8 +120,8 @@ async function updateUser(req, res, next) {
 async function authorize(req, res, next) {
   try {
     // 1. витягнути токен користувача з заголовка Authorization
-    const authorizationHeader = req.get("Authorization") || "";
-    const token = authorizationHeader.replace("Bearer ", "");
+    const authorizationHeader = req.get('Authorization') || '';
+    const token = authorizationHeader.replace('Bearer ', '');
 
     // 2. витягнути id користувача з пейлоада або вернути користувачу
     // помилку зі статус кодом 401
@@ -134,7 +129,7 @@ async function authorize(req, res, next) {
     try {
       userId = await jwt.verify(token, process.env.JWT_SECRET).id;
     } catch (err) {
-      next(new UnauthorizedError("User not authorized"));
+      next(new UnauthorizedError('User not authorized'));
     }
 
     // 3. витягнути відповідного користувача. Якщо такого немає - викинути
@@ -143,7 +138,7 @@ async function authorize(req, res, next) {
     const user = await usersModel.findById(userId);
 
     if (!user || user.token !== token) {
-      throw new UnauthorizedError("User not authorized");
+      throw new UnauthorizedError('User not authorized');
     }
 
     // 4. Якщо все пройшло успішно - передати запис користувача і токен в req
@@ -154,20 +149,17 @@ async function authorize(req, res, next) {
     next();
   } catch (err) {
     next(err);
-  } 
+  }
 }
 
-async function logout(req,res,next) {
-  try{
+async function logout(req, res, next) {
+  try {
     const user = req.user;
     await usersModel.updateToken(user._id, null);
-
     return res.status(204).send();
+  } catch (err) {
+    next(err);
   }
-  catch(err){
-    next(err)
-  }
-  
 }
 
 function validateCreateUser(req, res, next) {
@@ -183,7 +175,6 @@ function validateCreateUser(req, res, next) {
 
   next();
 }
-
 
 function validateSignIn(req, res, next) {
   const signInRules = Joi.object({
@@ -213,9 +204,6 @@ function validateChangeFieldUser(req, res, next) {
   next();
 }
 
-
-
-
 function validateId(req, res, next) {
   const { id } = req.params;
 
@@ -227,18 +215,14 @@ function validateId(req, res, next) {
 }
 
 function getSomeField(users) {
+  const filterUsers = users.map(user => ({
+    email: user.email,
+    subscription: user.subscription,
+    id: user._id,
+  }));
 
-  const filterUsers=users.map(user=>({
-    email:user.email,
-    subscription:user.subscription,
-    id:user._id
-  }))
-
-  return filterUsers
-
-  }
-  
-
+  return filterUsers;
+}
 
 module.exports = {
   addNewUser,
@@ -252,5 +236,6 @@ module.exports = {
   signIn,
   validateSignIn,
   authorize,
-  logout,getCurrentUser
+  logout,
+  getCurrentUser,
 };
--- api/users/user.router.js
+++ api/users/user.router.js
@@ -13,19 +13,18 @@ const {
   signIn,
   authorize,
   logout,
-  getCurrentUser
+  getCurrentUser,
 } = require('./user.controller.js');
 
 const userRouter = express.Router();
+
 userRouter.post('/auth/register', validateCreateUser, addNewUser);
-
 userRouter.get('/', getUsers);
-userRouter.delete('/:id',validateId, deleteUser);
-userRouter.get('/current',authorize,getCurrentUser);
-userRouter.get('/:id',validateId, getUserById);
+userRouter.delete('/:id', validateId, deleteUser);
+userRouter.get('/current', authorize, getCurrentUser);
+userRouter.get('/:id', validateId, getUserById);
 userRouter.post('/login', validateSignIn, signIn);
-userRouter.post("/auth/logout", authorize, logout);
+userRouter.post('/auth/logout', authorize, logout);
 userRouter.put('/:id', validateChangeFieldUser, updateUser);
-
 
 module.exports = userRouter;
